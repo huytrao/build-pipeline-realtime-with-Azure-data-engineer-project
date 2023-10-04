b30f96bea0f5e4fe402dcf44f818a35c1fe4db94
Huy Trao <121539558+huytrao@users.noreply.github.com>
2023-10-04 12:00:04 +0700

    Add files via upload

---
+++ SQL File/Sp_CreateSQLserver.sql
@@ -0,0 +1,19 @@
+USE gold_db
+GO 
+
+CREATE OR ALTER PROC CreateSQLServerlessView_gold @ViewName nvarchar(100)
+AS
+BEGIN
+
+DECLARE @statement VARCHAR(MAX)
+    SET @statement = N'CREATE OR ALTER VIEW ' + @ViewName + ' AS
+    SELECT *
+    FROM
+        OPENROWSET(
+            BULK ''https://****.dfs.core.windows.net/gold/SalesLT/' + @ViewName + '/'',
+            FORMAT = ''DELTA''
+        ) as [result]
+    '
+EXEC (@statement)
+END 
+GO
